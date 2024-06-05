0697b503bcfefe5230a6337c39351f18dcc7e52d
Mvke-IT <michael.duarte09@gmail.com>
2024-06-05 13:52:53 -0400

    created five new variables

---
+++ Let&Const.js
@@ -0,0 +1,11 @@
+const userId = 23
+let userName = 7
+let progress = 100
+let xp = 100
+let verified = true
+
+console.log(userId)
+console.log(userName)
+console.log(progress)
+console.log(xp)
+console.log(verified)
