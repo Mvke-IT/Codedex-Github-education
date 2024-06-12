95784179a3bb81a5f365c9472c986c046638b615
Mvke-IT <michael.duarte09@gmail.com>
2024-06-12 16:51:15 -0400

    converted an integer into its equivalent

---
+++ The_Console/based.numbers.js
@@ -0,0 +1,11 @@
+let myNumber = 51
+let binary = 'm'
+while (myNumber > 0)
+{
+  if(myNumber%2 ===0)
+  binary = '0' + binary;
+ else
+  binary ='1' + binary;
+  myNumber = Math.floor(myNumber/2);
+}
+console.log (binary)
